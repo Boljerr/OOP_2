640c80ecb8b8dd504aa83a33570c20f1fcd10d37
Boljerr <ignas.simaitis06@gmail.com>
2026-02-04 17:15:03 +0200

    Improved output

--- Objektinis/Objektinis/Objektinis.cpp
+++ Objektinis/Objektinis/Objektinis.cpp
@@ -1,19 +1,23 @@
+//ReSharper disable CommentTypo
+//ReSharper disable IdentifierTypo
+//ReSharper disable All
 // Objektinis.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include<iostream>
-#include <string>
-#include <vector>
+#include<string>
+#include<vector>
 #include<algorithm>
+#include<iomanip>
+
 struct Studentas {
 	std::string vardas;
 	std::string pavarde;
 	//int* pazymiai;
 	std::vector<int> pazymiai;
 	int egzaminas;
-	double rezultatas;
+	double Rezultatas;
 };
 
-#include <iostream>
 
 int main()
 {
@@ -39,28 +43,45 @@ int main()
 	if(n%2 == 1)
 	{
 		median = A.pazymiai[n / 2];
-		std::cout << "Mediana: " << median << "\n";
+	//	std::cout << "Mediana: " << median << "\n";
 	}
 	else
 	{
 		median = (A.pazymiai[n / 2 - 1] + A.pazymiai[n / 2]) / 2.0;
-		std::cout << "Mediana: " << median << "\n";
+		//std::cout << "Mediana: " << median << "\n";
 	}
 	std::cout << "Iveskite egzamino rezultata: ";
 	std::cin >> A.egzaminas;
 
 	average = total / n;
-	std::cout << "Vardas: " << A.vardas << "\nPavarde: " << A.pavarde << "\n";
-	std::cout << "Atsakymai:\n";
-
-	for (auto b : A.pazymiai)
-		std::cout << b << " ";
-	std::cout << "\nVidurkis: " << average << "\n";
-
-	A.rezultatas = 0.4 * average + 0.6 * A.egzaminas;
-	std::cout << "Galutinis rezultatas: " << A.rezultatas << "\n";
-	A.rezultatas = 0.4 * median + 0.6 * A.egzaminas;
-	std::cout << "Galutinis rezultatas (mediana): " << A.rezultatas << "\n";
+//	std::cout << "Atsakymai:\n";
+		
+//	for (auto b : A.pazymiai)
+	//	std::cout << b << " ";
+	std::cout << "Ar rezultata skaiciuojama su mediana ar su vidurkiu\n";
+	std::cout << "1 - vidurkis\n";
+	std::cout << "2 - mediana\n";
+	int choice;
+	std::string rez = "";
+	std::cin >> choice;
+	if (choice == 1)
+	{
+		A.Rezultatas = 0.4 * average + 0.6 * A.egzaminas;
+		rez = "Galutinis (Vid.)";
+		
+	}
+	else if (choice == 2)
+	{
+		A.Rezultatas = 0.4 * median + 0.6 * A.egzaminas;
+		rez = "Galutinis (Med.)";
+	}
+	else
+	{
+		std::cout << "Neteisingas pasirinkimas\n";
+		return 0;
+	}
+	std::cout << "Pavarde " << "Vardas " << rez	<< "\n";
+	std::cout << A.pavarde << " " << A.vardas << "	   " << std::fixed << std::setprecision(2) << A.Rezultatas << "\n";
 	
 }
 
