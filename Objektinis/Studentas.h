4e069f912508a6b12e25865422bd4b73de858080
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-05-12 13:20:07 +0300

    Convert Studentas structure to temporary class

--- Objektinis/Studentas.h
+++ Objektinis/Studentas.h
@@ -1,11 +1,40 @@
 #pragma once
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
 #include <string>
 #include <vector>
 
-struct Studentas {
+
+class Studentas
+{
+public:
 	std::string vardas;
 	std::string pavarde;
-	std::vector<int> pazymiai;
+	std::vector<int> namuDarbai;
 	int egzaminas;
 	double rezultatas;
+
+
+	Studentas();
+	Studentas(std::string& vardas, std::string& pavarde);
+	~Studentas();
+
+	std::string getVardas() const;
+	std::string getPavarde() const;
+	const std::vector<int>& getNamuDarbai() const;
+	int getEgzaminas() const;
+	double getRezultatas() const;
+
+
+	void setVardas(std::string& vardas);
+	void setPavarde(std::string& pavarde);
+	void setNamuDarbai(std::vector<int>& namuDarbai);
+	void setEgzaminas(int egzaminas);
+	void setRezultatas(double rezultatas);
+
+	void pridetiNamuDarba(int pazymys);
+	
 };
+
+#endif // STUDENTAS_H
