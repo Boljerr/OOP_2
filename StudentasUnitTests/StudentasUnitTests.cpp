2f85cb70e250656fff1ee4dd16ebde4a596d5c71
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-05-14 23:31:40 +0300

    Generate Unit test file

---
+++ StudentasUnitTests/StudentasUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StudentasUnitTests
+{
+	TEST_CLASS(StudentasUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
