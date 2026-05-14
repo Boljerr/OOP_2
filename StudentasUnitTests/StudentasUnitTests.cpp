91fba9fc7788e2ff97a678dac4928c4eb3ce2caa
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-05-15 00:00:51 +0300

    Add input operator unit test

--- StudentasUnitTests/StudentasUnitTests.cpp
+++ StudentasUnitTests/StudentasUnitTests.cpp
@@ -2,7 +2,7 @@
 #include "CppUnitTest.h"
 #include "Studentas.h"
 #include <utility>
-
+#include <sstream>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace StudentasUnitTests
@@ -78,6 +78,22 @@ namespace StudentasUnitTests
 			Assert::IsTrue(antras.getPazymiai()[0] == 8);
 			Assert::IsTrue(antras.getPazymiai()[1] == 9);
 			Assert::IsTrue(antras.getPazymiai()[2] == 10);
+		}
+		TEST_METHOD(InputOperatorWorks)
+		{
+			std::stringstream input("Jonas Jonaitis 8 9 10 9");
+			Studentas studentas;
+
+			input >> studentas;
+
+			Assert::IsTrue(studentas.getVardas() == "Jonas");
+			Assert::IsTrue(studentas.getPavarde() == "Jonaitis");
+			Assert::IsTrue(studentas.getEgzaminas() == 9);
+
+			Assert::IsTrue(studentas.getPazymiai().size() == 3);
+			Assert::IsTrue(studentas.getPazymiai()[0] == 8);
+			Assert::IsTrue(studentas.getPazymiai()[1] == 9);
+			Assert::IsTrue(studentas.getPazymiai()[2] == 10);
 		}
 	};
 }
