8b3552ffe7c466e52429362247050f8756af4e43
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-05-14 23:55:01 +0300

    Add move constructor unit test

--- StudentasUnitTests/StudentasUnitTests.cpp
+++ StudentasUnitTests/StudentasUnitTests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "Studentas.h"
+#include <utility>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -37,6 +38,21 @@ namespace StudentasUnitTests
 			Studentas antras;
 
 			antras = pirmas;
+
+			Assert::IsTrue(antras.getVardas() == "Jonas");
+			Assert::IsTrue(antras.getPavarde() == "Jonaitis");
+			Assert::IsTrue(antras.getEgzaminas() == 9);
+			Assert::IsTrue(antras.getPazymiai().size() == 3);
+
+			Assert::IsTrue(antras.getPazymiai()[0] == 8);
+			Assert::IsTrue(antras.getPazymiai()[1] == 9);
+			Assert::IsTrue(antras.getPazymiai()[2] == 10);
+		}
+		TEST_METHOD(MoveConstructorWorks)
+		{
+			Studentas pirmas("Jonas", "Jonaitis", {8, 9, 10}, 9);
+			
+			Studentas antras(std::move(pirmas));
 
 			Assert::IsTrue(antras.getVardas() == "Jonas");
 			Assert::IsTrue(antras.getPavarde() == "Jonaitis");
