1ab3f3ffe4a4fdcdbaaacea6ce9b139025370dca
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-05-14 23:48:39 +0300

    Add copy constructor unit test

--- StudentasUnitTests/StudentasUnitTests.cpp
+++ StudentasUnitTests/StudentasUnitTests.cpp
@@ -16,5 +16,21 @@ namespace StudentasUnitTests
 			Assert::IsTrue(true);
 		}
 
+		TEST_METHOD(CopyConstructorWorks)
+		{
+			Studentas pirmas("Jonas", "Jonaitis", {8, 9, 10}, 9);
+			
+			Studentas antras(pirmas);
+			
+			Assert::IsTrue(antras.getVardas() == "Jonas");
+			Assert::IsTrue(antras.getPavarde() == "Jonaitis");
+			Assert::IsTrue(antras.getEgzaminas() == 9);
+			Assert::IsTrue(antras.getPazymiai().size() == 3);
+
+			Assert::IsTrue(antras.getPazymiai()[0] == 8);
+			Assert::IsTrue(antras.getPazymiai()[1] == 9);
+			Assert::IsTrue(antras.getPazymiai()[2] == 10);
+		}
+
 	};
 }
